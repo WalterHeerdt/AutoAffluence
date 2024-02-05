b5d0340dfd5db0911cbf568f6a0b4c6709f783dc
WalterHeerdt <123971030+WalterHeerdt@users.noreply.github.com>
2024-02-05 15:05:27 +0100

    Update resa.py
    modification pour ajout de profils plus simples

--- Resa/resa.py
+++ Resa/resa.py
@@ -1,46 +1,51 @@
 import requests
 from datetime import datetime, timedelta
 
-def send_reservation_request(start_time, end_time, person_count, email, user_device_token):
+def send_reservation_requests(email_token_id_triples, start_time, end_time, person_count):
     # Obtenez la date actuelle formatée en YYYY-MM-DD
     date = (datetime.now() + timedelta(days=1)).strftime('%Y-%m-%d')
 
-    payload = {
-        "auth_type": None,
-        "email": email,
-        "date": date,
-        "start_time": start_time,
-        "end_time": end_time,
-        "note": None,
-        "user_firstname": None,
-        "user_lastname": None,
-        "user_phone": None,
-        "person_count": person_count,
-        "user_device_token": user_device_token
-    }
-
     headers = {
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:121.0) Gecko/20100101 Firefox/121.0",
+        "User-Agent": "",
         "Accept": "application/json, text/plain, */*",
         "Accept-Language": "fr",
         "Content-Type": "application/json",
     }
 
-    url = "https://reservation.affluences.com/api/reserve/49142"
-    response = requests.post(url, json=payload, headers=headers)
+    for email, user_device_token, reserve_id in email_token_id_triples:
+        # Construire l'URL avec l'ID spécifique
+        url = f"https://reservation.affluences.com/api/reserve/{reserve_id}"
+        
+        payload = {
+            "auth_type": None,
+            "email": email,
+            "date": date,
+            "start_time": start_time,
+            "end_time": end_time,
+            "note": None,
+            "user_firstname": None,
+            "user_lastname": None,
+            "user_phone": None,
+            "person_count": person_count,
+            "user_device_token": user_device_token
+        }
 
-    if response.status_code == 200:
-        print(f"Reservation success for {start_time} to {end_time}: ", response.json())
-    else:
-        print(f"Reservation failed for {start_time} to {end_time}: ", response.status_code, response.text)
+        response = requests.post(url, json=payload, headers=headers)
 
-# Informations de réservation
-email = 'leo.rocheteau1@etu.univ-nantes.fr'
-user_device_token = '36e6ef75-e52c-41cf-8614-ff123b65e06e'
+        if response.status_code == 201:
+            print(f"Reservation success for {email} from {start_time} to {end_time} with ID {reserve_id}: ", response.json())
+        else:
+            print(f"Reservation failed for {email} from {start_time} to {end_time} with ID {reserve_id}: ", response.status_code, response.text)
+
+# Liste de triples email-token-ID
+email_token_id_triples = [
+    ('leo.rocheteau1@etu.univ-nantes.fr', '36e6ef75-e52c-41cf-8614-ff123b65e06e', '49142'),
+    
+    # Ajoutez plus de triples email-token-ID ici
+]
+
 person_count = 1
 
-# Effectuer les réservations pour la date du jour
-send_reservation_request('10:00', '14:00', person_count, email, user_device_token)
-send_reservation_request('14:30', '18:30', person_count, email, user_device_token)
-send_reservation_request('19:00', '22:00', person_count, email, user_device_token)
-
+# Effectuer les réservations pour chaque triple
+for start_time, end_time in [('10:00', '14:00'), ('14:30', '18:30'), ('19:00', '22:00')]:
+    send_reservation_requests(email_token_id_triples, start_time, end_time, person_count)
