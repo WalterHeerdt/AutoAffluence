f3593616208f0e0f5b046e59c0b4c06d4e9e6271
WalterHeerdt <123971030+WalterHeerdt@users.noreply.github.com>
2024-04-06 09:06:05 +0200

    Update resa.py

--- Resa/resa.py
+++ Resa/resa.py
@@ -43,7 +43,9 @@ print(f"Current time: {current_time}")
 # Liste de triples email-token-ID
 email_token_id_triples = [
     ('leo.rocheteau1@etu.univ-nantes.fr', '71b7777d-5360-4583-b7fd-0f6b7f9a2bd1', '49142'), 
-    # Expiration: "2024-04-01"
+    # Expiration: "2024-05-02"
+    ('fanny.deckert@etu.univ-nantes.fr', 'e90ff558-f905-4f1f-a463-0695bcb6754a', '49363'), 
+    # Expiration: "2024-05-06"
     #('sixtine.audousset@etu.univ-nantes.fr', '99092a1a-a473-420c-a5fc-5853897490e7', '49128'),
     # Expiration: "2024-03-06"
     #('clarisse.lubrano@etu.univ-nantes.fr', 'ab271895-d8d8-4225-9dc5-b2c24f312a02', '49129'),
